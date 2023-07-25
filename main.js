664e2179b6d87fb6d81c1816d1b93be5c5a09982
TaehwanKim <taehwan642@naver.com>
2023-07-26 01:04:11 +0900

    fixing bug - scene1 404

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { runScene } from './scene_logic/scene1';
+import { runScene } from './scene_logic/scene1.js';
 
 const leftContainer = document.getElementById('left');
 
