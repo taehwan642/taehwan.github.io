af29133f3f10296a52731800b51d0327a1447fde
TaehwanKim <taehwan642@naver.com>
2023-08-01 22:07:16 +0900

    updating radiosity - subdivide

--- scene_logic/radiosity.js
+++ scene_logic/radiosity.js
@@ -1,48 +1,151 @@
 import * as THREE from 'three';
 import exampleScene from './exampleScene.js'
 
-export default class radiosity extends exampleScene {
+class Vector3 {
+    x;
+    y;
+    z;
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    toList() {
+        return [this.x, this.y, this.z];
+    }
+}
+
+class ModelData {
+    indices;
+    normal;
+    patchLevel;
+    elementLevel;
+    area;
+    reflectance;
+    emission;
+    constructor(indices, normal, patchLevel, elementLevel, area, reflectance, emission) {
+        this.indices = indices;
+        this.normal = normal;
+        this.patchLevel = patchLevel;
+        this.elementLevel = elementLevel;
+        this.area = area;
+        this.reflectance = reflectance;
+        this.emission = emission;
+    }
+}
+
+class Patch {
+    reflectance;
+    emission;
+    center;
+    normal;
+    unShotRadiosity;
+    area;
+    constructor(reflectance, emission, center, normal, unShotRadiosity, area) {
+        this.reflectance = reflectance;
+        this.emission = emission;
+        this.center = center;
+        this.normal = normal;
+        this.unShotRadiosity = unShotRadiosity;
+        this.area = area;
+    }
+}
+
+class Element {
     mesh;
-    // 1. input mesh, reflectance, emission factors
-    // 2. subdivide mesh (patch divide)
-    // 3. form factor calculation
-    //  i. use render each hemicube's faces by the eye of patch (z buffer)
-    //      a. at rendering, set pixel color to patch id (triangle id, mesh id)
-    //      b. to set id, set like this : (meshid + 1, floor (i / 255), 
-    //          i - (255 * floor (i / 255))), wile i = for loop to
-    //          index array length / 3
-    //      c. but use my own tri id, mesh id code
-    //  ii. after getting the rendered screen by rendertarget, 
-    //      the each pixel is going to represent tri id after calc
-    //  iii. calculate form factors
-    // 4. radiosity equation solving
-    //  i. iterate?
-    // 5. render
+    indices;
+    normal;
+    radiosity;
+    area;
+    parentPatch;
+    reflectance;
+    constructor(mesh, indices, normal, radiosity, area, parentPatch, reflectance) {
+        this.mesh = mesh;
+        this.indices = indices;
+        this.normal = normal;
+        this.radiosity = radiosity;
+        this.area = area;
+        this.parentPatch = parentPatch;
+        this.reflectance = reflectance;
+    }
+}
 
+class RadiosityParameter {
+    threshhold;
+    patchCount;
+    patches;
+    elementCount;
+    elements;
+    pointCount;
+    points;
+    displayView;
+    hemiCubeResolution;
+    worldSize;
+    intensityScale;
+    addAmbient;
+}
+
+export default class radiosity extends exampleScene {
+    elements;
     initialize(clientWidth, clientHeight) {
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera( 75, clientWidth / clientHeight, 0.1, 1000 );
+        const models = [];
+        const subDiviedVertices = [];
+        const patches = [];
+        const elements = [];
 
-        this.camera.position.z = 500;
-        this.camera.position.y = 50;
-        this.camera.position.x = 150;
+        this.initModelDatas(models);
+        let patchTotalCount = 0;
+        for (let i = 0; i < 19; ++i) {
+            patchTotalCount += models[i].patchLevel * models[i].patchLevel;
+        }
+        for (let i = 0; i < patchTotalCount; ++i) {
+            patches[i] = new Patch();
+        }
 
-        const light = new THREE.AmbientLight( 0xffffff ); // soft white light
-        this.scene.add( light );
+        let vertexOffset = 0;
+        let patchesIndex = 0;
+        let elementIndex = 0;
+        for (let i = 0; i < 19; ++i) {
+            const indexResult = this.subDivide(models[i], subDiviedVertices, patches, elements, vertexOffset, patchesIndex, elementIndex);
+            vertexOffset = indexResult.vertexOffset;
+            patchesIndex = indexResult.patchesIndex;
+            elementIndex = indexResult.elementIndex;
+            console.log(vertexOffset);
+            console.log(patchesIndex);
+            console.log(elementIndex);
+        }
 
-        for (var i = 0; i < 19; ++i) {
+        console.log(subDiviedVertices);
+
+        for (let i = 0; i < elements.length; ++i) {
             const geo = new THREE.BufferGeometry();
-            geo.setAttribute( 'position', new THREE.Float32BufferAttribute( this.getVerticesPosition(), 3 ) );
-            geo.setAttribute( 'normal', new THREE.Float32BufferAttribute( this.getNormal(i), 3 ) );
-            geo.setIndex(this.getIndices(i));
+            geo.setAttribute( 'position', new THREE.Float32BufferAttribute( subDiviedVertices, 3 ) );
+            geo.setAttribute( 'normal', new THREE.Float32BufferAttribute( elements[i].normal, 3 ) );
+            geo.setIndex(elements[i].indices);
 
             const material = new THREE.MeshBasicMaterial({
-                color: new THREE.Color(Math.random(), Math.random(), Math.random()),
+                color: new THREE.Color(elements[i].reflectance),
+                wireframe: true,
             });
 
             const mesh = new THREE.Mesh(geo, material);
-            this.mesh = mesh;
-            this.scene.add(mesh)
+
+            elements[i].mesh = mesh;
+        }
+        this.elements = elements;
+
+
+        this.initRadiosityParameter();
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, clientWidth / clientHeight, 0.1, 1000 );
+
+        this.camera.position.z = 700;
+        this.camera.position.y = 50;
+        this.camera.position.x = 150;
+
+        for (var i = 0; i < elements.length; ++i) {
+            this.scene.add(elements[i].mesh)
         }
 
         console.log(this.scene);
@@ -50,6 +153,10 @@ export default class radiosity extends exampleScene {
     processInput() {
     }
     update() {
+        for (let i = 0; i < this.elements.length; ++i) {
+            this.elements[i].mesh.rotation.x += 0.01;
+            this.elements[i].mesh.rotation.y += 0.01;
+        }
     }
     getDescription() {
         return `
@@ -57,7 +164,7 @@ export default class radiosity extends exampleScene {
         `;
     }
 
-    getVerticesPosition() {
+    getVerticesRawPosition() {
         return [
             0, 0, 0,
             216, 0, 0,
@@ -93,52 +200,170 @@ export default class radiosity extends exampleScene {
         ];
     }
 
-    getIndices(modelIndex) {
-        const indices = [];
-        indices[0] = [4, 5, 6, 4, 6, 7];
-        indices[1] = [0, 3, 2, 0, 2, 1];
-        indices[2] = [0, 4, 7, 0, 7, 3];
-        indices[3] = [0, 1, 5, 0, 5, 4];
-        indices[4] = [2, 6, 5, 2, 5, 1];
-        indices[5] = [2, 3, 7, 2, 7, 6];
-        indices[6] = [8, 9, 10, 8, 10, 11];
-        indices[7] = [16, 19, 18, 16, 18, 17];
-        indices[8] = [12, 13, 14, 12, 14, 15];
-        indices[9] = [12, 15, 19, 12, 19, 16];
-        indices[10] = [12, 16, 17, 12, 17, 13];
-        indices[11] = [14, 13, 17, 14, 17, 18];
-        indices[12] = [14, 18, 19, 14, 19, 15];
-        indices[13] = [24, 27, 26, 24, 26, 25];
-        indices[14] = [20, 21, 22, 20, 22, 23];
-        indices[15] = [20, 23, 27, 20, 27, 24];
-        indices[16] = [20, 24, 25, 20, 25, 21];
-        indices[17] = [22, 21, 25, 22, 25, 26];
-        indices[18] = [22, 26, 27, 22, 27, 23];
-        return indices[modelIndex];
+    getVerticesPosition() {
+        return [
+            new Vector3(0, 0, 0),
+            new Vector3(216, 0, 0),
+            new Vector3(216, 0, 215),
+            new Vector3(0, 0, 215),
+            new Vector3(0, 221, 0),
+	        new Vector3(216, 221, 0),
+	        new Vector3(216, 221, 215),
+	        new Vector3(0, 221, 215),
+	        new Vector3(85.5, 220, 90),
+	        new Vector3(130.5, 220, 90),
+	        new Vector3(130.5, 220, 130),
+	        new Vector3(85.5, 220, 130),
+	        new Vector3(53.104, 0, 64.104),
+	        new Vector3(109.36, 0, 96.604),
+	        new Vector3(76.896, 0, 152.896),
+	        new Vector3(20.604, 0, 120.396),
+	        new Vector3(53.104, 65, 64.104),
+	        new Vector3(109.36, 65, 96.604),
+	        new Vector3(76.896, 65, 152.896),
+	        new Vector3(20.604, 65, 120.396),
+	        new Vector3(134.104, 0, 67.104),
+	        new Vector3(190.396, 0, 99.604),
+	        new Vector3(157.896, 0, 155.896),
+	        new Vector3(101.604, 0, 123.396),
+	        new Vector3(134.104, 130, 67.104),
+	        new Vector3(190.396, 130, 99.604),
+	        new Vector3(157.896, 130, 155.896),
+	        new Vector3(101.604, 130, 123.396)
+        ];
     }
 
-    getNormal(modelIndex) {
-        const normals = [];
-        normals[0] = [0, -1, 0];
-        normals[1] = [0, -1, 0];
-        normals[2] = [0, 1, 0];
-        normals[3] = [1, 0, 0];
-        normals[4] = [0, 0, 1];
-        normals[5] = [-1, 0, 0];
-        normals[6] = [0, 0,-1];
-        normals[7] = [0, -1, 0];
-        normals[8] = [0, 1, 0];
-        normals[9] = [0, -1, 0];
-        normals[10] = [-0.866, 0, -0.5];
-        normals[11] = [0.5, 0, -0.866];
-        normals[12] = [0.866, 0, 0.5];
-        normals[13] = [-0.5, 0, 0.866];
-        normals[14] = [0, 1, 0];
-        normals[15] = [0, -1, 0];
-        normals[16] = [-0.866, 0, -0.5];
-        normals[17] = [0.5, 0, -0.866];
-        normals[18] = [0.866, 0, 0.5];
-        normals[19] = [-0.5, 0, 0.866];
-        return normals[modelIndex];
+    initModelDatas(models) {
+        const red = new THREE.Color( 0.80, 0.10, 0.075 );
+        const yellow = new THREE.Color( 0.9, 0.8, 0.1 );
+        const blue = new THREE.Color( 0.075, 0.10, 0.35 );
+        const white = new THREE.Color( 1.0, 1.0, 1.0 );
+        const lightGrey = new THREE.Color( 0.9, 0.9, 0.9 );
+        const black = new THREE.Color( 0.0, 0.0, 0.0 );
+        const lightGreen = new THREE.Color( 0.63, 0.85, 0.58 )
+
+        models[0] = new ModelData([4, 5, 6, 4, 6, 7],           [0, -1, 0],         1, 3, (216 * 215),    lightGrey,    black);
+        models[1] = new ModelData([0, 3, 2, 0, 2, 1],           [0, 1, 0],          1, 3, (216 * 215),    lightGrey,    black);
+        models[2] = new ModelData([0, 4, 7, 0, 7, 3],           [1, 0, 0],          1, 3, (221 * 215),    red,          black);
+        models[3] = new ModelData([0, 1, 5, 0, 5, 4],           [0, 0, 1],          1, 3, (221 * 216),    lightGrey,    black);
+        models[4] = new ModelData([2, 6, 5, 2, 5, 1],           [-1, 0, 0],         1, 3, (221 * 215),    blue,         black);
+        models[5] = new ModelData([2, 3, 7, 2, 7, 6],           [0, 0, -1],         1, 3, (221 * 216),    lightGrey,    black);
+        models[6] = new ModelData([8, 9, 10, 8, 10, 11],        [0, -1, 0],         1, 3, (40 * 45),      black,        white);
+        models[7] = new ModelData([16, 19, 18, 16, 18, 17],     [0, 1, 0],          1, 3, (65 * 65),      yellow,       black);
+        models[8] = new ModelData([12, 13, 14, 12, 14, 15],     [0, -1, 0],         1, 3, (65 * 65),      yellow,       black);
+        models[9] = new ModelData([12, 15, 19, 12, 19, 16],     [-0.866, 0, -0.5],  1, 3, (65 * 65),      yellow,       black);
+        models[10] = new ModelData([12, 16, 17, 12, 17, 13],    [0.5, 0, -0.866],   1, 3, (65 * 65),      yellow,       black);
+        models[11] = new ModelData([14, 13, 17, 14, 17, 18],    [0.866, 0, 0.5],    1, 3, (65 * 65),      yellow,       black);
+        models[12] = new ModelData([14, 18, 19, 14, 19, 15],    [-0.5, 0, 0.866],   1, 3, (65 * 65),      yellow,       black);
+        models[13] = new ModelData([24, 27, 26, 24, 26, 25],    [0, 1, 0],          1, 3, (65 * 65),      lightGreen,   black);
+        models[14] = new ModelData([20, 21, 22, 20, 22, 23],    [0, -1, 0],         1, 3, (65 * 65),      lightGreen,   black);
+        models[15] = new ModelData([20, 23, 27, 20, 27, 24],    [-0.866, 0, -0.5],  1, 3, (65 * 130),     lightGreen,   black);
+        models[16] = new ModelData([20, 24, 25, 20, 25, 21],    [0.5, 0, -0.866],   1, 3, (65 * 130),     lightGreen,   black);
+        models[17] = new ModelData([22, 21, 25, 22, 25, 26],    [0.866, 0, 0.5],    1, 3, (65 * 130),     lightGreen,   black);
+        models[18] = new ModelData([22, 26, 27, 22, 27, 23],    [-0.5, 0, 0.866],   1, 3, (65 * 130),     lightGreen,   black);
     }
+
+    convertUVtoPoint(vertices, u, v)
+    {
+        let point = new Vector3(
+            vertices[0].x * (1 - u) * (1 - v) + vertices[1].x * (1 - u) * v + vertices[2].x * u * v + vertices[3].x * u * (1 - v),
+            vertices[0].y * (1 - u) * (1 - v) + vertices[1].y * (1 - u) * v + vertices[2].y * u * v + vertices[3].y * u * (1 - v),
+            vertices[0].z * (1 - u) * (1 - v) + vertices[1].z * (1 - u) * v + vertices[2].z * u * v + vertices[3].z * u * (1 - v),
+        ); 
+        return point;
+    }
+
+    subDivide(modelData, subDiviedVertices, patches, elements, vertexOffset, patchesIndex, elementIndex) {
+        let quadVertices = [];
+        let nu, nv;
+        let	du, dv;
+        let i, j;
+        let u, v;
+        let fi, fj;
+        let pi, pj;
+        let verticesCount = 0;
+
+        quadVertices[0] = this.getVerticesPosition()[modelData.indices[0]];
+        quadVertices[1] = this.getVerticesPosition()[modelData.indices[1]];
+        quadVertices[2] = this.getVerticesPosition()[modelData.indices[2]];
+        quadVertices[3] = this.getVerticesPosition()[modelData.indices[5]];
+
+        // index is wrong. calculate index.
+        // vertex calculation is wrong also. 3rd vtx.
+        nu = modelData.patchLevel * modelData.elementLevel + 1;
+        nv = modelData.patchLevel * modelData.elementLevel + 1;
+        du = 1.0 / (nu - 1);
+        dv = 1.0 / (nv - 1);
+        for (i = 0, u = 0; i < nu; i++, u += du) {
+            for (j = 0, v = 0; j < nv; j++, v += dv, verticesCount++) {
+                console.log(du + " " + dv + " " + u + " " + v);
+                subDiviedVertices.push(...this.convertUVtoPoint(quadVertices, u, v).toList());
+            }
+        }
+
+        nu = modelData.patchLevel * modelData.elementLevel;
+        nv = modelData.patchLevel * modelData.elementLevel;
+        du = 1.0 / nu; dv = 1.0 / nv;
+        for (i = 0, u = du / 2.0; i < nu; i++, u += du) {
+            for (j = 0, v = dv / 2.0; j < nv; j++, v += dv, elementIndex++) {
+                fi = i / nu;
+                fj = j / nv;
+                pi = Math.trunc(fi * (modelData.patchLevel));
+                pj = Math.trunc(fj * (modelData.patchLevel));
+
+                elements[elementIndex] = new Element(
+                    null,
+                    [
+                        (i * (nv + 1) + j) + vertexOffset,
+                        ((i + 1) * (nv + 1) + j) + vertexOffset,
+                        ((i + 1) * (nv + 1) + (j + 1)) + vertexOffset,
+                        (i * (nv + 1) + j) + vertexOffset,
+                        ((i + 1) * (nv + 1) + (j + 1)) + vertexOffset,
+                        (i * (nv + 1) + (j + 1)) + vertexOffset
+                    ],
+                    modelData.normal,
+                    null,
+                    modelData.area / (nu * nv),
+                    patches[patchesIndex + pi * modelData.patchLevel + pj],
+                    modelData.reflectance,
+                );
+                const idxarray = [
+                    (i * (nv + 1) + j) + vertexOffset,
+                    ((i + 1) * (nv + 1) + j) + vertexOffset,
+                    ((i + 1) * (nv + 1) + (j + 1)) + vertexOffset,
+                    (i * (nv + 1) + j) + vertexOffset,
+                    ((i + 1) * (nv + 1) + (j + 1)) + vertexOffset,
+                    (i * (nv + 1) + (j + 1)) + vertexOffset
+                ];
+                console.log(idxarray);
+            }
+        }
+
+        /* Calculate patches */
+        nu = modelData.patchLevel;
+        nv = modelData.patchLevel;
+        du = 1.0 / nu;
+        dv = 1.0 / nv;
+        for (i = 0, u = du / 2.0; i < nu; i++, u += du) {
+            for (j = 0, v = dv / 2.0; j < nv; j++, v += dv, patchesIndex++) {
+                patches[patchesIndex].center = this.convertUVtoPoint(quadVertices, u, v);
+                patches[patchesIndex].normal = modelData.normal;
+                patches[patchesIndex].reflectance = modelData.reflectance;
+                patches[patchesIndex].emission = modelData.emission;
+                patches[patchesIndex].area = modelData.area / (nu * nv);
+            }
+        }
+        vertexOffset += verticesCount;
+
+        return {
+            vertexOffset: vertexOffset,
+            patchesIndex: patchesIndex,
+            elementIndex: elementIndex,
+        };
+    }
+
+    initRadiosityParameter() {
+
+    }
+
 }
